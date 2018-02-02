46f59cffa72266eb5c9235da82d04cbbd650171f
Shank <develop@zestic.com>
2018-02-02 00:16:53 -0800

    update namespace

--- src/AuthenticationMiddleware.php
+++ src/AuthenticationMiddleware.php
@@ -9,10 +9,10 @@ declare(strict_types=1);
 
 namespace Zend\Expressive\Authentication;
 
-use Interop\Http\Server\MiddlewareInterface;
-use Interop\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 class AuthenticationMiddleware implements MiddlewareInterface
 {
